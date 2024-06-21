283231c300686302c4e90b52afa6c89dc705f135
Oleh Hanenko <hello.isaev@gmail.com>
2024-06-22 01:32:32 +0300

    make build

---
+++ index.f4b7ef9e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.display="flex",e.style.alignItems="center",e.style.justifyContent="center",t.style.position="relative";
+//# sourceMappingURL=index.f4b7ef9e.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+wall.style.display = "flex";
+wall.style.alignItems = "center";
+wall.style.justifyContent = "center";
+spider.style.position = "relative";
+
+//# sourceMappingURL=index.f75de5e1.js.map
